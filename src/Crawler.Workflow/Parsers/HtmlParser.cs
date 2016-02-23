4fb3f9085b86ce21a51f322dff71fbb23397b943
iGilga <pyua87@gmail.com>
2016-02-23 07:16:50 +0300

    Added parsers

---
+++ src/Crawler.Workflow/Parsers/HtmlParser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace Crawler.Workflow.Parsers
+{
+    public class HtmlParser:IParser
+    {
+        public IEnumerable<string> GetUrls(string content)
+        {
+            ICollection<string> urls = new List<string>();
+
+            if (!string.IsNullOrWhiteSpace(content))
+            {
+                HtmlDocument doc = new HtmlDocument();
+
+                doc.LoadHtml(content);
+
+                var nodes = doc.DocumentNode.SelectNodes("//a[@href]");
+
+                if (nodes != null)
+                {
+                    foreach (HtmlNode node in nodes)
+                    {
+                        urls.Add(node.Attributes["href"].Value);
+                    }
+                }
+            }
+
+            return urls;
+        }
+    }
+}
---
+++ src/Crawler.Workflow/Parsers/IParser.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Crawler.Workflow.Parsers
+{
+    public interface IParser
+    {
+        IEnumerable<string> GetUrls(string content);
+    }
+}
