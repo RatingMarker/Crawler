bb2731fae41735f8f58e2b3cb7823651bbb6b78e
iGilga <pyua87@gmail.com>
2016-02-25 05:13:08 +0300

    Извлечения из файла config.json

---
+++ src/Crawler.Workflow/Configurations/AppConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Crawler.Workflow.Configurations
+{
+    public interface IAppConfiguration
+    {
+        string Get(string property);
+    }
+
+    public class AppConfiguration: IAppConfiguration
+    {
+        private readonly IConfiguration configuration;
+
+        public AppConfiguration()
+        {
+            var config = new ConfigurationBuilder();
+            config.AddJsonFile("config.json");
+
+            configuration = config.Build();
+        }
+
+        public string Get(string property)
+        {
+            return configuration[property];
+        }
+    }
+}
