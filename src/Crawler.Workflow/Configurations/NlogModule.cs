4b46106a9e213051a75a8e407f130e9fdf9c4d5d
iGilga <pyua87@gmail.com>
2016-02-25 05:13:27 +0300

    Создан логгер

---
+++ src/Crawler.Workflow/Configurations/NlogModule.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace Crawler.Workflow.Configurations
+{
+    public class NLogModule: Module
+    {
+        public NLogModule()
+        {
+            var config = new LoggingConfiguration();
+
+            var consoleTarget = new ColoredConsoleTarget
+            {
+                Layout = @"${time}|${level}|${message}"
+            };
+            config.AddTarget("console", consoleTarget);
+
+            var fileTarget = new FileTarget()
+            {
+                Layout = @"${time}|${level}|${message}",
+                FileName = "Log.txt"
+            };
+            config.AddTarget("file", fileTarget);
+
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Info, consoleTarget));
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, fileTarget));
+
+            LogManager.Configuration = config;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c => LogManager.GetLogger(GetType().Namespace)).As<ILogger>();
+        }
+    }
+}
