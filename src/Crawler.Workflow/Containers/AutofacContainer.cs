319474496646938dcb2a07e1deccdbd16a350f10
iGilga <pyua87@gmail.com>
2016-02-26 21:19:32 +0300

    Инициализация IoC

---
+++ src/Crawler.Workflow/Containers/AutofacContainer.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Crawler.Workflow.Configurations;
+
+namespace Crawler.Workflow.Containers
+{
+    public static class AutofacContainer
+    {
+        private static IContainer container;
+
+        public static IContainer Get()
+        {
+            return container ?? (container = Build());
+        }
+
+        private static IContainer Build()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<AutofacModule>();
+
+            return builder.Build();
+        }
+    }
+}
