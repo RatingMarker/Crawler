f07f228bfa725f4a0c5aebde870000101f99f79f
iGilga <pyua87@gmail.com>
2016-02-26 22:12:02 +0300

    Добавлено управление краулера

---
+++ src/Crawler.Workflow/CrawlerManage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Crawler.Workflow.Models;
+using Crawler.Workflow.Processings;
+using Crawler.Workflow.Services;
+using NLog;
+
+namespace Crawler.Workflow
+{
+    public class CrawlerManage
+    {
+        private readonly ICrawlerProcessing crawlerProcessing;
+        private readonly ILogger logger;
+        private readonly IStorageService storageService;
+        private bool isProcessing;
+        private IEnumerable<Site> sites;
+
+        public CrawlerManage(
+            ICrawlerProcessing crawlerProcessing,
+            IStorageService storageService,
+            ILogger logger)
+        {
+            if (crawlerProcessing == null)
+            {
+                throw new ArgumentNullException(nameof(crawlerProcessing));
+            }
+            if (storageService == null)
+            {
+                throw new ArgumentNullException(nameof(storageService));
+            }
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+            this.crawlerProcessing = crawlerProcessing;
+            this.storageService = storageService;
+            this.logger = logger;
+        }
+
+        public void InitializeCrawler()
+        {
+            ProcessingCrawler();
+        }
+
+        public void ProcessingCrawler()
+        {
+            isProcessing = true;
+
+            logger.Info("Crawler starting");
+
+            sites = storageService.GetSites();
+
+            ProcessingNewSites();
+
+            isProcessing = false;
+
+            logger.Info("Crawler completed");
+        }
+
+        private void ProcessingNewSites()
+        {
+            foreach (var site in sites)
+            {
+                var pages = storageService.GetPagesByState(site.SiteId, 1);
+
+                crawlerProcessing.InitializeProcession(pages);
+            }
+        }
+    }
+}
