4eaefade879900e1955d34f93169f200426a2755
iGilga <pyua87@gmail.com>
2016-02-26 20:17:32 +0300

    Добавлены модели

---
+++ src/Crawler.Workflow/Models/Rating.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Crawler.Workflow.Models
+{
+    public class Rating
+    {
+        public int RatingId { get; set; }
+        public int KeywordId { get; set; }
+        public int PageId { get; set; }
+        public int SiteId { get; set; }
+        public DateTime JoinDate { get; set; }
+    }
+}
