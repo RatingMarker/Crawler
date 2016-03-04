e1a8470c828a001d21d85c6560344d1c3656d6f4
iGilga <pyua87@gmail.com>
2016-03-05 02:14:04 +0300

    Deleted class

--- src/Crawler.Workflow/Models/Subword.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Crawler.Workflow.Models
-{
-    public class Subword
-    {
-        public int SubwordId { get; set; }
-        public string Name { get; set; }
-        public int KeywordId { get; set; }
-    }
-}
