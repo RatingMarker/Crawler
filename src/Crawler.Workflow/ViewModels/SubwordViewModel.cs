11819d8cee964cb371d4b426d0678ef88d2015fc
iGilga <pyua87@gmail.com>
2016-03-05 02:59:55 +0300

    Delete dto

--- src/Crawler.Workflow/ViewModels/SubwordViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Crawler.Workflow.ViewModels
-{
-    public class SubwordViewModel
-    {
-        public int SubwordId { get; set; }
-        public string Name { get; set; }
-        public int KeywordId { get; set; }
-    }
-}
